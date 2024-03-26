1ac0c609c9b2c84409bab8243311c0d4c551ddee
jofe2003 <jo.fe2003@usp.br>
2024-03-26 13:57:32 -0300

    transmissao de dados funcionando

--- serial.v
+++ serial.v
@@ -4,20 +4,31 @@ module serial (
 	output tx, //fio serial de transmissão
 	output done,
 	output busy,
-	output db_clock
+	output db_clock,
+	output db_botao
 );
 	// Want to interface to 9600 baud UART
 	// 50000000 / 9600 = 5208 Clocks Per Bit (clock 50 MHz)
-
+assign db_botao = ~enable;
 	
 uart_tx #(5208) UART_TX_INST (
 		.i_Clock(fpga_clock),
-		.i_Tx_DV(enable), //enable
+		.i_Tx_DV(wire_enable), //enable
 		.i_Tx_Byte(8'b01000001), 
 		.o_Tx_Active(busy), //busy
 		.o_Tx_Serial(tx),
 		.o_Tx_Done(done) // pronto
 	);
+	
+	wire wire_enable;
+  edge_detector edge_detector (
+    .clock(fpga_clock),
+    .reset(),
+    .sinal(enable),
+    .pulso(wire_enable)
+);
+	
+	
 	
 	assign db_clock = fpga_clock;
 	
