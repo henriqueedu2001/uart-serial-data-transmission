bd8e05ec64b4eea66fd8cbc814768427e9ef00ce
jofe2003 <jo.fe2003@usp.br>
2024-03-26 16:40:43 -0300

    recebimento e transmissão funcionando

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import serial
 
 # Configurar a porta serial (substitua '/dev/ttyUSB0' pelo seu dispositivo serial)
-serial_port = serial.Serial('COM4', baudrate=9600, timeout=1)
+serial_port = serial.Serial('COM4', baudrate=9600, timeout=1, parity=serial.PARITY_NONE)
 
 try:
     while True:
-        # Ler 1 byte (8 bits) da porta serial
+        # Espera por entrada do usuário para enviar dados
+        data_to_send = input("Digite os dados a serem enviados (pressione Enter para enviar): ")
+        
+        # Verifica se os dados não estão vazios
+        if data_to_send:
+            # Envie os dados pela porta serial
+            serial_port.write(data_to_send.encode())  # Converta a entrada de string para bytes antes de enviar
+            
+            # Confirme o envio
+            print("Dados enviados com sucesso:", data_to_send)
+        
+        # Leia 1 byte (8 bits) da porta serial
         byte_received = serial_port.read(1)
         
         if byte_received:
-            # Exibir o byte recebido
+            # Exiba o byte recebido
             print("Byte recebido:", byte_received.hex())  # Exibe o byte como hexadecimal
 except KeyboardInterrupt:
     # Se o usuário pressionar Ctrl+C, feche a porta serial e encerre o programa
--- serial_tb.v
+++ serial_tb.v
@@ -1,36 +1,37 @@
 `timescale 1ns/1ns
 module serial_tb;
-	
-reg fpga_clock;
-reg enable;
-wire done;
-wire busy;
-wire tx;
-wire db_clock;
+	reg clock;
+	reg iniciar_transmissao;
+	reg entrada_serial; // para o receptor serial
+	wire saida_serial; // fio serial de transmissão para o transmissor
+	wire transmissor_acabou;
+	wire transmissor_ocupado;
+	wire [6:0] hex0_out;
+	wire [6:0] hex1_out;
+	wire db_botao;
+	wire receptor_acabou;
 
 serial uut(
-	.fpga_clock(fpga_clock),
-	.enable(enable),
-	.done(done),
-	.busy(busy),
-	
-	.tx(tx), //fio serial de transmissão
-	.db_clock(db_clock)
+	.clock(clock),
+	.iniciar_transmissao(iniciar_transmissao),
+	.entrada_serial(entrada_serial), // para o receptor serial
+	.saida_serial(saida_serial), // fio serial de transmissão para o transmissor
+	.transmissor_acabou(transmissor_acabou),
+	.transmissor_ocupado(transmissor_ocupado),
+	.hex0_out(hex0_out),
+	.hex1_out(hex1_out),
+	.db_botao(db_botao),
+	.receptor_acabou(receptor_acabou)
 );
 
+
 always
-	#5 fpga_clock = !fpga_clock;
+	#5 clock = !clock;
 
 	initial begin
 		$dumpfile("wave.vcd");
         $dumpvars(5, serial_tb);
-		fpga_clock = 0;
-		enable = 1;
-		#1000
-
-		enable = 0;
-
-		#1000000;
+		
 		$finish;
 	end
 endmodule 
